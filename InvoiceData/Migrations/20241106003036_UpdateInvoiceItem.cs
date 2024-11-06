976b00145a630b0fb817a86f2f50af993ef05347
George E. Hassan III <ghassan_iii@hotmail.com>
2024-11-05 19:32:55 -0500

    EF cleanup

---
+++ InvoiceData/Migrations/20241106003036_UpdateInvoiceItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceData.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateInvoiceItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "InvoiceItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "InvoiceItem");
+        }
+    }
+}
--- InvoiceData/Models/InvoiceItem.cs
+++ InvoiceData/Models/InvoiceItem.cs
@@ -2,6 +2,8 @@ using Newtonsoft.Json;
 
 public class InvoiceItem
 {
+    public int Id { get; set; } // Surrogate key
+
     [JsonProperty("description")]
     public string? Description { get; set; }
 
@@ -13,4 +15,6 @@ public class InvoiceItem
 
     [JsonProperty("amount")]
     public decimal? Amount { get; set; }
+
+    public string InvoiceId { get; set; } // Foreign key to Invoice
 }
